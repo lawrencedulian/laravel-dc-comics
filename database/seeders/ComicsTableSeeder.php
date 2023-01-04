860297dc9b2a77ac2d9d0ef8163b2e8d9b714ea8
lawrencedulian <lawrenceedulian@gmail.com>
2023-01-04 12:06:59 +0100

    added soft delete

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -19,14 +19,14 @@ class ComicsTableSeeder extends Seeder
 
         foreach ($comicsData as $comic) {
             $newComic = new Comic();
-            $newComic->fill($comic);
-            // $newComic->title = $comic['title'];
-            // $newComic->description = $comic['description'];
-            // $newComic->thumb = $comic['thumb'];
-            // $newComic->price = $comic['price'];
-            // $newComic->series = $comic['series'];
-            // $newComic->sale_date = $comic['sale_date'];
-            // $newComic->type = $comic['type'];
+            // $newComic->fill($comic);
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
             $newComic->save();
         }
     }
